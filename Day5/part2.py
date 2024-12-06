b62f22696ce25476571bacc8e3ddf97148c0e7a0
noccu <37862870+noccu@users.noreply.github.com>
2024-12-06 04:09:44 +0100

    Day 5 but a bit less stupid

--- Day5/part2.py
+++ Day5/part2.py
@@ -1,5 +1,5 @@
 from math import floor
-from functools import reduce
+from functools import reduce, cmp_to_key
 
 
 def get_input():
@@ -25,38 +25,31 @@ def get_update_rules(update, rules):
     return relevant_rules
 
 
-def get_must_precede_list(page, upd_rules):
-    x = tuple((r[1] for r in upd_rules if r[0] == page))
-    # print(f"Page {page} must precede:", x)
-    return x
+def get_bidir_pages(page, upd_rules):
+    fol = [r[1] for r in upd_rules if r[0] == page]
+    prec = [r[0] for r in upd_rules if r[1] == page]
+    return prec, fol
 
 
 def validate_update(update, upd_rules):
-    # print(f"Checking update {update}")
     for idx, page in enumerate(update):
-        must_precede = get_must_precede_list(page, upd_rules)
-        preceding_pages: tuple = update[:idx]
-        for mp_pg in must_precede:
+        _, fol = get_bidir_pages(page, upd_rules)
+        preceding_pages = update[:idx]
+        for mp_pg in fol:
             if mp_pg in preceding_pages:
-                return False, (idx, mp_pg)
-    return True, (None, None)
+                return False
+    return True
 
 
-def fix_update_still_dumb(upd, rules):
-    upd_rules = get_update_rules(upd, rules)
-    sort_map = {p: i for i, p in enumerate(upd)}
-    for pg, i in sort_map.items():
-        for following_page in get_must_precede_list(pg, upd_rules):
-            if following_page not in sort_map:
-                continue
-            if sort_map[following_page] > sort_map[pg]:
-                continue
-            sort_map[following_page] = sort_map[pg] + i
-    presorted_upd = list(sort_map.keys())
-    presorted_upd.sort(key=lambda x: sort_map[x])
-    if not validate_update(presorted_upd, upd_rules)[0]:
-        return fix_update_still_dumb(presorted_upd, rules)
-    return presorted_upd
+def cmp(upd_rules):
+    def _cmp(a,b):
+        prec, fol = get_bidir_pages(a, upd_rules)
+        if b in prec:
+            return 1
+        if b in fol:
+            return -1
+        return 0
+    return _cmp
 
 
 def middle(upd):
@@ -64,8 +57,10 @@ def middle(upd):
 
 
 all_rules, all_updates = get_input()
-bad_updates = filter(lambda u: not validate_update(u, all_rules)[0], all_updates)
-res = reduce(
-    lambda acc, u: acc + middle(u), map(lambda u: fix_update_still_dumb(u, all_rules), bad_updates), 0
-)
+
+bad_updates = list(filter(lambda u: not validate_update(u, all_rules), all_updates))
+cmp_all = cmp(all_rules)
+for upd in bad_updates:
+    upd.sort(key=cmp_to_key(cmp_all))
+res = reduce(lambda acc, u: acc + middle(u), bad_updates, 0)
 print(res)
