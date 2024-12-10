981f9b4ec3af8f177dabde08d11a2d5c190254fe
noccu <37862870+noccu@users.noreply.github.com>
2024-12-10 01:22:35 +0100

    Day 9 small class refactor + clarify var names

--- Day9/part1.py
+++ Day9/part1.py
@@ -3,18 +3,19 @@ from time import process_time
 
 
 @dataclass
-class FreeSpace:
+class Data:
     size: int
+    id: int = None
 
+    def is_empty(self):
+        return self.id is None
 
-@dataclass
-class File:
-    size: int
-    id: int = 0
+    def is_file(self):
+        return not self.is_empty()
 
 
 def get_input():
-    with open("input.txt", "r") as f:
+    with open("test_input.txt", "r") as f:
         return f.readline().strip()
 
 
@@ -26,39 +27,38 @@ def analyse_disk(encoded_blocks: str):
     for i in range(0, len(encoded_blocks)):
         size = encoded_blocks[i]
         if is_file:
-            data = File(size, file_id)
+            data = Data(size, file_id)
             file_id += 1
         else:
-            data = FreeSpace(size)
+            data = Data(size)
         for _ in range(0, size):
             disk_map.append(data)
         is_file = not is_file
     return disk_map
 
 
-def find_free_space(disk_map: list, idx=0):
-    i = idx
-    while i < len(disk_map):
-        data_at_block = disk_map[i]
-        if isinstance(data_at_block, FreeSpace):
+def find_empty(disk_map: list[Data], idx=0):
+    while idx < len(disk_map):
+        data = disk_map[idx]
+        if data.is_empty():
             # print(f"Found free space at {i}: {data_at_block}")
-            return i, data_at_block
-        i += 1
+            return idx, data
+        idx += 1
     return None, None
 
 
-def defrag(disk_map: list):
-    free_space_idx = 0
+def defrag(disk_map: list[Data]):
+    empty_idx = 0
     for i in range(len(disk_map) - 1, -1, -1):
         block = disk_map[i]
-        if isinstance(block, FreeSpace):
+        if block.is_empty():
             continue
-        free_space_idx, data_ref = find_free_space(disk_map, free_space_idx)
-        if data_ref is None or free_space_idx >= i:
+        empty_idx, empty_data = find_empty(disk_map, empty_idx)
+        if empty_data is None or empty_idx >= i:
             break
-        disk_map[free_space_idx] = block
-        data_ref.size = data_ref.size - 1
-        disk_map[i] = FreeSpace(1)
+        disk_map[empty_idx] = block
+        empty_data.size = empty_data.size - 1
+        disk_map[i] = Data(1)
         # print(f"Moved file {block.id} to {free_space_idx}")
 
 
@@ -67,9 +67,8 @@ res = 0
 disk_map = analyse_disk(get_input())
 defrag(disk_map)
 for i, block in enumerate(disk_map):
-    if isinstance(block, FreeSpace):
-        continue
-    res += i * block.id
+    if block.is_file():
+        res += i * block.id
 end = process_time()
 print(res)
 print(f"Took {end-start}s")
