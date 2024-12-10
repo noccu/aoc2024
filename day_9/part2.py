0c36fca4de9c049efd3cbd7fc04d285c3dd7c5ce
noccu <37862870+noccu@users.noreply.github.com>
2024-12-11 00:01:06 +0100

    Create general solver

--- day_9/part2.py
+++ day_9/part2.py
@@ -3,14 +3,13 @@ from time import process_time
 
 
 @dataclass
-class FreeSpace:
+class Data:
     size: int
-
-
-@dataclass
-class File:
-    size: int
-    id: int = 0
+    id: int = None
+    def is_empty(d):
+        return d.id is None
+    def is_file(d):
+        return not d.is_empty()
 
 
 def get_input():
@@ -26,44 +25,43 @@ def analyse_disk(encoded_blocks: str):
     for i in range(0, len(encoded_blocks)):
         size = encoded_blocks[i]
         if is_file:
-            data = File(size, file_id)
+            data = Data(size, file_id)
             file_id += 1
         else:
-            data = FreeSpace(size)
+            data = Data(size)
         for _ in range(0, size):
             disk_map.append(data)
         is_file = not is_file
     return disk_map
 
 
-def find_free_space(disk_map: list, req_size):
-    i = 0
-    while i < len(disk_map):
-        data_at_block = disk_map[i]
-        if isinstance(data_at_block, FreeSpace) and data_at_block.size >= req_size:
+def find_empty(disk_map: list[Data], req_size, idx=0):
+    while idx < len(disk_map):
+        data = disk_map[idx]
+        if data.is_empty() and data.size >= req_size:
             # print(f"Found free space at {i}: {data_at_block}")
-            return i, data_at_block
-        i += data_at_block.size
+            return idx, data
+        idx += data.size
     return None, None
 
 
-def defrag(disk_map: list):
-    free_space_idx = 0
+def defrag(disk_map: list[Data]):
+    empty_idx = 0
     i = len(disk_map) - 1
     while i > -1:
         block = disk_map[i]
         i -= block.size
-        first_block_idx = i + 1
-        if isinstance(block, FreeSpace):
+        if block.is_empty():
             continue
-        free_space_idx, data_ref = find_free_space(disk_map, block.size)
-        if data_ref is None or free_space_idx >= first_block_idx:
+        block_idx = i + 1
+        empty_idx, data_ref = find_empty(disk_map, block.size)
+        if data_ref is None or empty_idx >= block_idx:
             continue
         data_ref.size = data_ref.size - block.size
-        new_freespace = FreeSpace(block.size)
+        new_freespace = Data(block.size)
         for offset in range(0, block.size):
-            disk_map[free_space_idx + offset] = block
-            disk_map[first_block_idx + offset] = new_freespace
+            disk_map[empty_idx + offset] = block
+            disk_map[block_idx + offset] = new_freespace
         # print(f"Moved file {block.id} to {free_space_idx}")
 
 
---
+++ solve.py
@@ -0,0 +1,11 @@
+from sys import argv
+from os import chdir
+from importlib import import_module
+
+if len(argv) == 3:
+    day = argv[1]
+    part = argv[2]
+    print(f"Result of day {day}, part {part}:")
+    day_dir = f"day_{day}"
+    chdir(day_dir)
+    import_module(f"{day_dir}.part{part}")
